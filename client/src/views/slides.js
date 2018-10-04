4a89727c5fea02fa8613b4a10398f36fb822a267
eric leong <you@example.com>
2018-10-04 16:02:48 -0700

    completed slides on front page

--- client/src/views/slides.js
+++ client/src/views/slides.js
@@ -2,38 +2,31 @@ import React from 'react';
 import Slider from "react-slick";
 import "../../../node_modules/slick-carousel/slick/slick.css";
 import "../../../node_modules/slick-carousel/slick/slick-theme.css";
-import homeImage from '../assets/home.png';
 
 class SimpleSlider extends React.Component {
   render() {
     var settings = {
         infinite: true,
-        speed: 500,
-        slidesToShow: 4,
+        speed: 1500,
+        slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
         dots : true,
-        autoplaySpeed: 100
+        autoplaySpeed: 3500
     };
     return (
       <Slider {...settings}>
-        {/**}<div>
-        <img alt="" src={require("../assets/logo_main.png")}/>
-        </div>**/}
         <div>
-          <img alt = "pic" src = {`https://robohash.org/0 `}/>
+        <img alt="" src={require('../assets/home.png')}/>
         </div>
         <div>
-          <img alt = "pic" src = {`https://robohash.org/1 `}/>
+          <img alt = "pic" src={require('../assets/members.jpg')}/>
         </div>
         <div>
-          <img alt = "pic" src = {`https://robohash.org/2 `}/>
+          <img alt = "pic" src={require('../assets/industry.png')}/>
         </div>
         <div>
-          <img alt = "pic" src = {`https://robohash.org/3 `}/>
-        </div>
-        <div>
-          <img alt = "pic" src = {`https://robohash.org/4 `}/>
+          <img alt = "pic" src={require('../assets/micromouse.png')}/>
         </div>
       </Slider>
     );
