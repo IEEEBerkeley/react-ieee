9916a845b3bcfbe647f7810e46f565c975382a32
eric leong <you@example.com>
2018-10-06 11:37:27 -0700

    added google calendar

--- client/src/views/Calendar.jsx
+++ client/src/views/Calendar.jsx
@@ -2,92 +2,43 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 import eventsImage from '../assets/home.png';
+class Googlecal extends Component {
+    constructor() {
+        super();
+    }
+    iframe() {
+        return {__html: this.props.iframe};
+    }
+    render() {
+        return (
+          <div>
+            <div dangerouslySetInnerHTML={ this.iframe() } />
+          </div>
+        );
+    }
+}
+const iframe = '<iframe src="https://calendar.google.com/calendar/b/1/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=berkeley.edu_9k08nrpdahaujiva7u1ss9j92s%40group.calendar.google.com&amp;color=%238D6F47&amp;ctz=America%2FLos_Angeles" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>';
 
 class Calendar extends Component {
-  constructor() {
-    super();
-    this.currentPage = 1;
-    this.state = {numEvents: 10};
-    let self = this;
-  }
-
-  componentDidMount() {
-    this.changePage(1, null);
-    this.setState(previousState => {
-      return { numEvents: document.getElementById('eventList').childNodes.length }
-    })
-  }
-
-  range(n) {
-    let arr = [];
-    for (let i = 1; i <= n; i++) {
-      arr.push(i);
-    }
-    return arr;
-  }
-
-  changePage(n, e) {
-    let newPage;
-    let numEvents = this.state.numEvents;
-
-    if (e == null) newPage = n;
-    else {
-      if (e.target.innerText == 'Next ›') {
-        newPage = this.currentPage + 1;
-      } else {
-        newPage = parseInt(e.target.innerText);
-      }
-    }
-    if (newPage > Math.ceil(numEvents / 4)) return;
-
-    let arr = document.getElementById('eventList').getElementsByClassName('event');
-    let steps = 0;
-
-    for (let i = 0; i < (newPage - 1) * 4; i++) {
-      steps += 1;
-      arr[i].style.display = 'none';
-    }
-
-    if (steps < arr.length)
-      arr[steps].style.display = 'block';
-    if (steps + 1 < arr.length)
-      arr[steps + 1].style.display = 'block';
-    if (steps + 2 < arr.length)
-      arr[steps + 2].style.display = 'block';
-    if (steps + 3 < arr.length)
-      arr[steps + 3].style.display = 'block';
-
-    for (let i = steps + 4; i < arr.length; i++) {
-      arr[i].style.display = 'none';
-    }
-
-    this.currentPage = newPage;
-  }
 
   render() {
-    let self = this;
-    return (
-      <div>
-        <div className="hero"
-        style={{ backgroundImage: "url(" + eventsImage + ")" }}>
+      let self = this;
+      return (
+        <div>
+          <div className="hero"
+          style={{ backgroundImage: "url(" + eventsImage + ")" }}>
+          </div>
+          <div className="submenu">
+          <ul>
+            <Link to={"/events"}><li className="">All Events</li></Link>
+            <Link to={"/Calendar"}><li className="">Calendar</li></Link>
+          </ul>
         </div>
-        <div className="submenu">
-        <ul>
-          <Link to={"/industry"}><li className="">All Events</li></Link>
-          {/*<Link to={"/industry/donate"}><li className="">Photos</li></Link>*/}
-        </ul>
-      </div>
       <div class="content">
-      <div class="left">
-        <span class="heading blue">All Events</span>
-
-        <div id="eventList">
-
-
-
-        </div>
-
-        </div>
+     
+      <div class = "left">
+      <Googlecal iframe={iframe} />
+      </div>
 
         </div>
       </div>
--- client/src/views/Events.jsx
+++ client/src/views/Events.jsx
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 import eventsImage from '../assets/home.png';
 
+const iframe = '<iframe src="https://calendar.google.com/calendar/b/1/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=berkeley.edu_9k08nrpdahaujiva7u1ss9j92s%40group.calendar.google.com&amp;color=%238D6F47&amp;ctz=America%2FLos_Angeles" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>';
+
+class Calendar extends Component {
+    constructor() {
+        super();
+    }
+    iframe() {
+        return {__html: this.props.iframe};
+    }
+    render() {
+        return (
+          <div>
+            <div dangerouslySetInnerHTML={ this.iframe() } />
+          </div>
+        );
+    }
+}
+
+
 class Events extends Component {
   constructor() {
     super();
@@ -73,8 +92,8 @@ class Events extends Component {
         </div>
         <div className="submenu">
         <ul>
-          {/*<Link to={"/events"}><li className="">All Events</li></Link>*/}
-          {/*<Link to={"/industry/donate"}><li className="">Photos</li></Link>*/}
+          <Link to={"/events"}><li className="">All Events</li></Link>
+          <Link to={"/events/Calendar"}><li className="">Calendar</li></Link>
         </ul>
       </div>
       <div class="content">
