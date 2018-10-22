33e771c1decc89c8ddcb71de8a4fa7a647f097f8
Peru Dayani <perudayani@berkeley.edu>
2018-10-22 16:11:02 -0700

    Micromouse Update

--- client/src/views/Micromouse.jsx
+++ client/src/views/Micromouse.jsx
@@ -15,7 +15,14 @@ class Micromouse extends Component {
         Micromouse
         </span>
         <p>
-        <br />The UC Berkeley IEEE Student Branch’s Micromouse DeCal is a hands-on course aimed at undergraduates with an interest in robotics. In this class, teams of 3~5 students are formed to build and program autonomous, maze-solving cars that follow the standards set in IEEE’s Micromouse competition. <br /> <br /> The course assumes no experience and will be based around a series of labs and project milestones that cover a wide range of robotics concepts. The course will expose students to Arduino programming, autonomous navigation, sensors, PID, and basic electrical engineering, while preparing them for an in-class competition at the end of each semester. Teams are also provided with the opportunity to qualify for funding to attend competitions among other schools in California and neighboring states.Visit <a class="blue" href="https://decal.berkeley.edu">https://decal.berkeley.edu</a> for enrollment details.
+        <br />
+        The UC Berkeley IEEE Student Branch’s Micromouse DeCal is a hands-on course aimed at undergraduates with an interest in robotics. In the class, teams of 2~5 students are formed to build and program autonomous, maze-solving cars that follow the standards set in IEEE’s Micromouse competition.
+        <br />
+        <br />
+         The course assumes no experience and will be based around a series of labs and project milestones that cover a wide range of robotics concepts. The course will expose students to Arduino programming, autonomous navigation, sensors, PID, and basic electrical engineering, while preparing them for an in-class competition at the end of each semester. Teams are also provided with the opportunity to qualify for funding to attend competitions among other schools in California and neighboring states.
+         <br />
+         <br />
+          All necessary parts will be provided to students at no cost.
         </p>
 
         </div>
