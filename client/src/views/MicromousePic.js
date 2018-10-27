9bb35527cc1b8b554a4c44c68320ea59d9b3ea73
eric leong <you@example.com>
2018-10-27 03:21:23 -0700

    added micromouse slider

---
+++ client/src/views/MicromousePic.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Slider from "react-slick";
+import "../../../node_modules/slick-carousel/slick/slick.css";
+import "../../../node_modules/slick-carousel/slick/slick-theme.css";
+
+class SimpleSlider extends React.Component {
+  render() {
+    var settings = {
+        infinite: true,
+        speed: 1500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots : false,
+        autoplaySpeed: 3500,
+        dots: true,
+        variablewidth: true
+    };
+    return (
+      <Slider {...settings}>
+        <div>
+        <img alt="" src={require('../assets/micromouse/micromouse1.jpg')}/>
+        </div>
+        <div>
+        <img alt="" src={require('../assets/micromouse/micromouse7.jpg')}/>
+        </div>
+        <div>
+        <img alt="" src={require('../assets/micromouse/micromouse2.jpg')}/>
+        </div>
+        <div>
+        <img alt="" src={require('../assets/micromouse/micromouse3.jpg')}/>
+        </div>
+        <div>
+        <img alt="" src={require('../assets/micromouse/micromouse4.jpg')}/>
+        </div>
+        <div>
+        <img alt="" src={require('../assets/micromouse/micromouse5.jpg')}/>
+        </div>
+        <div>
+        <img alt="" src={require('../assets/micromouse/micromouse6.jpg')}/>
+        </div>
+      </Slider>
+    );
+  }
+}
+export default SimpleSlider;
