c88f7ce9526afc74072fb0d3dcc9b38e34a4b5b6
ewendai <yiwendai99@berkeley.edu>
2019-03-13 01:11:23 -0700

    Add content to Apply Page.

--- client/src/views/Apply.jsx
+++ client/src/views/Apply.jsx
@@ -1,38 +1,57 @@
 import React, { Component } from 'react';
-import micromouseImage from '../assets/micromouse.png';
 import memberImage from '../assets/members.jpg';
 class Apply extends Component {
   render() {
     return (
       <div>
         <html>
-        <body>
-            <div className="hero"
-                style={{ backgroundImage: "url(" + memberImage + ")" }}>
-            </div>
-        <div class='content'>
-        <span class='heading blue'>
-        Join IEEE!
-        </span>
-        <p>
-        <br />
-        <p> Apply
-        <a href="https://tinyurl.com/ucbieeesp19" class="blue" target="_blank"> here</a>.
-        </p>
-        Contact us for more information.
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        </p>
+	        <body>
+	            <div className="hero"
+	                style={{ backgroundImage: "url(" + memberImage + ")" }}>
+	            </div>
 
-        </div>
-        </body>
+		        <div class='content'>
+			        <span class='heading blue'>
+			        Join Us @ UC Berkeley IEEE!
+			        </span>
+			        <p>
+				        <br />
+				        <p> Apply
+				        <a href="https://tinyurl.com/ucbieeesp19" class="blue" target="_blank"> here </a> to become a General Member.
+				        </p>
+				    </p>
+
+				    <br />
+
+			        <span class='heading blue'>
+			        Member Requirements
+			        </span>
+					<p>
+						<br />
+
+						<p>
+				        Events will be announced on Facebook and on Slack in addition to being posted on our calendar. Don't forget to sign-in at each event to indicate attendance.
+				        </p>
+
+				        <br />
+				        <p> 
+				        General Members: 
+				        	<li> Attend 1 Social Event & 2 Academic/Professional Events </li>
+				        <br />
+				        Committee/Leadership: 
+				        	<li> Volunteer for 1 hour at the Startup Fair/Soda Hacks </li>
+				        	<li> Volunteer for 3 hours of Service/Outreach events or cookie runs </li>
+				        	<li> Volunteer for one finals breakfast (replaceable with 2 hours of additional service/outreach) </li>
+				        </p>
+
+				        <br />
+
+				        <p>
+				        These requirements are to encourage you to support the EECS community and IEEE's major initiatives, and each of these events will be a great opportunity to meet your peers within IEEE!
+				        </p>
+			        </p>
+		        </div>
+	        </body>
         </html>
       </div>
     );
