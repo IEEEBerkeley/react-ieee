a167f40e9c24d8daf5e29f99a39d337344bb40b7
Ahad Rauf <ahadrauf@gmail.com>
2019-11-10 00:05:39 -0800

    laid the foundations of an Auth0-based Google authentication

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,36 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Auth0Provider } from "./react-auth0-spa";
+import config from "./auth_config.json";
+
+// A function that routes the user to the right place
+// after login
+const onRedirectCallback = appState => {
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
+//ReactDOM.render(
+//<BrowserRouter>
+//<App />
+//</BrowserRouter>, document.getElementById('root'));
 
 ReactDOM.render(
-<BrowserRouter>
-<App />
-</BrowserRouter>, document.getElementById('root'));
+  <Auth0Provider
+    domain={config.domain}
+    client_id={config.clientId}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+>
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
+
 registerServiceWorker();
